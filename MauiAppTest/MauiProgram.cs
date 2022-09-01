5fb75c05bb2403ff0ba1d63c20f5a7d2429e1ee4
rezaicando <reza.icando@gmail.com>
2022-08-31 18:04:44 -0700

    Add project files.

---
+++ MauiAppTest/MauiProgram.cs
@@ -0,0 +1,26 @@
+﻿using MauiApp1.ViewModel;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace MauiApp1;
+
+public static class MauiProgram
+{
+	public static MauiApp CreateMauiApp()
+	{
+		var builder = MauiApp.CreateBuilder();
+		builder
+			.UseMauiApp<App>()
+			.ConfigureFonts(fonts =>
+			{
+				fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+				fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
+			});
+
+		builder.Services.AddSingleton<MainPage>(); //singleton is global 
+		builder.Services.AddSingleton<MainViewModel>();
+
+        builder.Services.AddTransient<DetailPage>(); //transient is temperary and each time creates it
+        builder.Services.AddTransient<DetailViewModel>();
+        return builder.Build();
+	}
+}
---
+++ MauiAppTest/ViewModel/MainViewModel.cs
@@ -0,0 +1,73 @@
+﻿
+
+
+using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using System.Collections.ObjectModel;
+
+namespace MauiApp1.ViewModel;
+
+
+
+public partial class MainViewModel :ObservableObject
+{
+    public MainViewModel()
+    {
+        items= new ObservableCollection<string>();
+    }
+
+
+    [ObservableProperty]
+    ObservableCollection<String> items;
+
+
+    [ObservableProperty]
+    public string text;
+
+    [RelayCommand]
+    void Add()
+    {
+        if(string.IsNullOrEmpty(text))
+            return;
+
+        Items.Add(text);
+        Text=string.Empty;
+    }
+    [RelayCommand]
+    void Delete(string item)
+    {
+        if (items.Contains(item))
+        {
+            items.Remove(item);
+        }
+    }
+    [RelayCommand]
+    async Task Tap(string s)
+    {
+        await Shell.Current.GoToAsync($"{nameof(DetailPage)}?Text={s}");
+        //await Shell.Current.GoToAsync($"{nameof(DetailPage)}?Text={s}",new Dictionary<String, object>
+        //{
+        //    {nameof(DetailPage),s},
+        //});
+    }
+   
+}
+
+
+//public class MainViewModel : INotifyPropertyChanged
+//{
+//    private string _text;
+//    public string Text
+//    {
+//        get => _text;
+//        set
+//        {
+//            _text = value;
+//            OnPropertyChanged(nameof(Text));
+//        }
+//    }
+//    void OnPropertyChanged(string name)=>
+//        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+
+//    public event PropertyChangedEventHandler PropertyChanged;
+//}
